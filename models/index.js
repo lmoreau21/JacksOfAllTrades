b3635549d8a9cbdb950dffb3a776565aa106b4c5
Lilly Moreau <lmoreau2021@gmail.com>
2022-09-20 22:31:50 -0500

    fixed error and index

--- models/index.js
+++ models/index.js
@@ -1,9 +1,16 @@
 import {SkillLink} from '../src/ui-components';
 
+import { initSchema } from '@aws-amplify/datastore';
+import { schema } from './schema';
+
+
+
+const { Skill } = initSchema(schema);
+
 export default function Home() {
   return(
     <div>
-      <SkillLink />
+      <SkillLink/>
     </div>
 
   )
