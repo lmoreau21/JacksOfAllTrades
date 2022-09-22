ad33297c4feb5b35b6c743470a020d2d6dac4995
Lilly Moreau <lmoreau2021@gmail.com>
2022-09-22 11:56:04 -0500

    added needed files

---
+++ node_modules/@aws-amplify/pubsub/lib-esm/index.d.ts
@@ -0,0 +1,16 @@
+import { PubSub } from './PubSub';
+export * from './Providers';
+declare enum CONTROL_MSG {
+    CONNECTION_CLOSED = "Connection closed",
+    CONNECTION_FAILED = "Connection failed",
+    REALTIME_SUBSCRIPTION_INIT_ERROR = "AppSync Realtime subscription init error",
+    SUBSCRIPTION_ACK = "Subscription ack",
+    TIMEOUT_DISCONNECT = "Timeout disconnect"
+}
+export { CONNECTION_STATE_CHANGE } from './Providers/constants';
+export { ConnectionState } from './types';
+export { PubSub, CONTROL_MSG };
+/**
+ * @deprecated use named import
+ */
+export default PubSub;
