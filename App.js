066d9384a7be63429ed37867581f2cebccc51029
Lilly Moreau <lmoreau2021@gmail.com>
2022-09-22 11:43:58 -0500

    add myamplify push

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 import {Amplify} from 'aws-amplify'
 import config from '../src/aws-exports'
 
 import {AmplifyProvider} from '@aws-amplify/ui-react'
 import { Component } from 'react';
-import {studioThem} from '../src/ui-components'
+import {studioTheme} from '../src/ui-components'
 
 import '@aws-amplify/ui-react/styles.css'
 
