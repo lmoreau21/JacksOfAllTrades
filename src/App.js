aebf55aa40e35bf40bb20ec725ed88bed51a6855
Lilly Moreau <lmoreau2021@gmail.com>
2022-09-22 22:43:12 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+import '../styles/globals.css'
+
 import {Amplify} from 'aws-amplify'
 import config from '../src/aws-exports'
 
 import {AmplifyProvider} from '@aws-amplify/ui-react'
-import { Component } from 'react';
+//import { Component } from 'react';
 import {studioTheme} from '../src/ui-components'
 
-import '@aws-amplify/ui-react/styles.css'
+import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(config)
 
-function App() {
-  return (
-    <AmplifyProvider theme={studioTheme}><Component {...pageProps}/></AmplifyProvider>)
-}
+function App({Component, pageProps}) {
+  return <AmplifyProvider theme={studioTheme}><Component {...pageProps}/></AmplifyProvider>;
+  }
 
 export default App;
