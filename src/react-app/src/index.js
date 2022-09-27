e8f9cc26b0ba15a6a0f794ec3760e67b6f382bee
AlecHodges <50474509+APH225@users.noreply.github.com>
2022-09-27 00:16:17 -0500

    Created a new app from my local machine, added it to the github and removed the old version.

--- src/react-app/src/index.js
+++ src/react-app/src/index.js
@@ -1,9 +1,8 @@
-/*
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './react-app/src/reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,16 +14,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-*/
-import {HeroLayout1} from "../ui-components";
-
-export default function Home (){
-  return (
-    <div>
-      <HeroLayout1/>
-    </div>
-  )
-}
-
 reportWebVitals();
