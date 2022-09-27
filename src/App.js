1f5935c49d66b2a20f022bfff60ea1b22ebff56b
Alec-Palmer-Hodges <111945515+Alec-Palmer-Hodges@users.noreply.github.com>
2022-09-27 18:49:53 -0500

    Made some text changes to see if the webapp will update

--- src/App.js
+++ src/App.js
@@ -7,17 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Welcome, I hope that this works!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Welcom to the Jacks Of All Trades Webapp: We hope that you like it
+        P.S: Alec is the Best! :)
       </header>
     </div>
   );
