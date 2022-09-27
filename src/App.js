96e4dc83aec3616ee1a1d95bb4cea997693863d1
AlecHodges <50474509+APH225@users.noreply.github.com>
2022-09-26 19:43:17 -0500

    Added @aws-amplify/ui-react

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import '../styles/globals.css'
 import {Amplify} from 'aws-amplify'
 import config from '../src/aws-exports'
 
-import {AmplifyProvider} from '../../node_modules/@aws-amplify/ui-react'
+import {AmplifyProvider} from '../node_modules/@aws-amplify/ui-react'
 //import { Component } from 'react';
 import {studioTheme} from '../src/ui-components'
 
