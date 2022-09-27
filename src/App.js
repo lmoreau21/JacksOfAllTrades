f9f4884911a0d4d964f3643a99891e3d3b8d4757
AlecHodges <50474509+APH225@users.noreply.github.com>
2022-09-26 21:30:52 -0500

    Fixing error code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import '../styles/globals.css'
 
-import {Amplify} from 'aws-amplify'
+import {Amplify} from './aws-amplify'
 import config from '../src/aws-exports'
 
 import {AmplifyProvider} from '@aws-amplify/ui-react'
