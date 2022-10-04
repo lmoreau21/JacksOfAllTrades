fbd0a1c2744bca1673f9ae2affb25aaae7ede3b4
Lilly Moreau <lmoreau2021@gmail.com>
2022-10-04 11:37:03 -0500

    pleas work take 3

--- jacksofalltrades/src/index.js
+++ jacksofalltrades/src/index.js
@@ -8,12 +8,13 @@ import config from './aws-exports'
 import Amplify from 'aws-amplify';
 import { AmplifyProvider } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css'
+import { Login } from './ui-components';
 
 Amplify.configure(config)
 
 ReactDOM.render(
   <AmplifyProvider>
-    <App />  
+    <Login />  
   </AmplifyProvider>,
   document.getElementById('root')
 );  
