f3879646d8901e0f9eab3759fcb30701bd6ac81b
Lilly Moreau <lmoreau2021@gmail.com>
2022-10-05 17:34:54 -0500

    add auth

--- jacksofalltrades/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ jacksofalltrades/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "jacksofalltrades9b9a0612": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
