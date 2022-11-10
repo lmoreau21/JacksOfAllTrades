d0c63905215e0db4c61ec72660c75442d6ea912d
Lilly Moreau <lmoreau2021@gmail.com>
2022-11-09 18:20:45 -0600

    in progress

--- jacksofalltrades/src/ui-components/ContactForm.js
+++ jacksofalltrades/src/ui-components/ContactForm.js
@@ -7,15 +7,23 @@ const ContactForm = () => {
   const onSubmit = (e) => {
     e.preventDefault()
     setFormStatus('Submitting...')
-    const { name, message } = e.target.elements
+    const { name, email, message } = e.target.elements
     let conFom = {
       name: name.value,
       message: message.value,
+      email: email.value
     }
+    
     console.log(conFom)
   }
 
   return (
+    <form
+        action="mailto:lmoreau2021@gmail.com"
+        method="Post"
+        enctype="text/plain"
+        name="EmailForm"
+        >
     <View 
         onSubmit={onSubmit}
         width="100%"
@@ -26,12 +34,6 @@ const ContactForm = () => {
         alignItems= 'center'
         backgroundImage="linear-gradient(45deg, rgba(167,83,83,1), rgba(167,153,153,1))"
         
-        >
-        <form
-            action="mailto:lmoreau2021@gmail.com"
-            method="Post"
-            enctype="text/plain"
-            name="EmailForm"
         >
         <Flex
             backgroundColor="#D3D3D3"
@@ -50,6 +52,7 @@ const ContactForm = () => {
              backgroundColor=""
         >Submit a new skill</Text>
         <Flex>
+            <input>
             <TextField className="field" 
                 type="text" 
                 id="name" 
@@ -60,9 +63,26 @@ const ContactForm = () => {
                 required>
 
             </TextField>
+            </input>
         </Flex>
         <Flex 
         >
+            <input>
+            <TextField 
+                className="field" 
+                id="email" 
+                placeholder="email" 
+                
+                textAlign="center" 
+                backgroundColor="rgba(255,255,255,.5)"
+                border="1px SOLID rgba(0,0,0,1)"
+                lineHeight="20px"
+                required />
+            </input>
+        </Flex>
+        <Flex 
+        >
+            <input>
             <TextField 
                 className="field" 
                 id="message" 
@@ -73,6 +93,7 @@ const ContactForm = () => {
                 border="1px SOLID rgba(0,0,0,1)"
                 lineHeight="20px"
                 required />
+            </input>
         </Flex>
         <Flex
             
@@ -91,8 +112,9 @@ const ContactForm = () => {
         </Button>
         </Flex>
         </Flex>
-        </form>
+        
     </View>
+    </form>
   )
 }
 export default ContactForm;
