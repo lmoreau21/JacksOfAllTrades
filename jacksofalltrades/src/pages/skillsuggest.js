9c1e0f8cd55cb1dc1b37c17d29001963f54344b9
Lilly Moreau <lmoreau2021@gmail.com>
2022-11-10 01:11:41 -0600

    centers contact form graphic with comment

--- jacksofalltrades/src/pages/skillsuggest.js
+++ jacksofalltrades/src/pages/skillsuggest.js
@@ -4,7 +4,7 @@ import { AmplifyProvider, withAuthenticator } from "@aws-amplify/ui-react";
 import ContactForm from "../ui-components/ContactForm";
 import { studioTheme } from "../ui-components";
 
-
+//calls contact form component
 function SkillSuggest() {
     return(
         <AmplifyProvider theme={studioTheme}  >
